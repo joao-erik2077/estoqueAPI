a7274ad3a2ec571c9590471c81a42c8f14e1b5f2
João Érik da Silva Crisóstomo <joao.crisostomo01@etec.sp.gov.br>
2022-05-23 08:31:51 -0300

    Initial commit

---
+++ src/test/java/br/com/joaoerik/estoqueAPI/EstoqueApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.joaoerik.estoqueAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstoqueApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
